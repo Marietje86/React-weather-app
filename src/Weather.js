2d5526ca30070438a778f44cc1ef6b45200a595d
Marietje86 <81444491+Marietje86@users.noreply.github.com>
2021-06-17 18:59:23 +0200

    fixing closing tags
    Adding API to search

--- src/Weather.js
+++ src/Weather.js
@@ -1,72 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./styles.css";
 import Search from "./Search";
 
+{/*   ******  Temperature ******  */}
 export default function Weather() {
-  let weatherData = {
-    city: "Barcelona",
-    country: "Spain",
-    date: "Thur, May 5",
-    time: "15:00",
-    description: "Sunny",
-    imgUrl: "http://openweathermap.org/img/wn/01d@2x.png",
-    temperature: 23,
-    feels_like: 25,
-    max_temp: 26,
-    min_temp: 21,
-    humidity: 72,
-    wind: 3
-  };
-  return (
-    <div className="Weather">
-      <div class="container">
-        <div class="row"></div>
-      </div>
-      <h1>
-        <span>😎</span>What weather you can expect<span>🤔</span>
-      </h1>
-      {/*   ******  Search ******  */}
-      <Search />
-      <br />
-      {/*   ******  Temperature ******  */}
+const [ready, setReady] = useState(false);
+const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    setTemperature(response.data.main.temp);
+    } 
+
+    let weatherData = {
+      city: "Barcelona",
+      country: "Spain",
+      date: "Thur, May 5",
+      time: "15:00",
+      description: "Sunny",
+      imgUrl: "http://openweathermap.org/img/wn/01d@2x.png",
+      temperature: 23,
+      feels_like: 25,
+      max_temp: 26,
+      min_temp: 21,
+      humidity: 72,
+      wind: 3
+    };
+
+    if (ready) {
+      return (
+        <div className="Weather">
+        <div class="container">
+          <div class="row"></div>
+        </div>
+        <h1>
+          <span>😎</span>What weather you can expect<span>🤔</span>
+        </h1>
+        {/*   ******  Search ******  */}
+        <Search />
+        <br />
       <div className="row justify-content-between current-weather-conditions">
-        <div className="col-6">
-          <div className="location">
-            {weatherData.city}, {weatherData.country}
-          </div>
-          <br />
-          <div className="date-and-time">
-            Last updated: {weatherData.date}, {weatherData.time}
-          </div>
-        </div>
-        <h2 id="temperature">
-          <a href="#" id="celsius"></a>
-        </h2>
-        <div class="clearfix">
-          <h3 id="show-city">Cloudy</h3>
-          <div className="description"></div>
-          <img
-            src="http://openweathermap.org/img/wn/04d@2x.png"
-            alt="clear"
-            id="icon"
-            class="float-left"
-            width="60"
-          />
-          <br />
+      <div className="col-6">
+        <div className="location">
+          {weatherData.city}, {weatherData.country}
         </div>
         <br />
-        <ul>
-          <div className="weather-app-wrapper">
-            <li className="col-3">Feels like: {weatherData.feels_like}°C</li>
-            <li className="col-3">
-              H/L: <span className="high-temp">{weatherData.max_temp}</span>°C
-              <span className="low-temp"> {weatherData.min_temp}</span>°C
-            </li>
-            <li className="col-3">Humidity: {weatherData.humidity}%</li>
-            <li className="col-3">Winds: {weatherData.wind} km/h</li>
-          </div>
-        </ul>
+        <div className="date-and-time">
+          Last updated: {weatherData.date}, {weatherData.time}
+        </div>
+      </div>
+      <h2 id="temperature">
+        <a href="#" id="celsius"></a>
+      </h2>
+      <div class="clearfix">
+        <h3 id="show-city">Cloudy</h3>
+        <div className="description"></div>
+        <img
+          src="http://openweathermap.org/img/wn/04d@2x.png"
+          alt="clear"
+          id="icon"
+          class="float-left"
+          width="60"
+        />
         <br />
+      </div>
+      <br />
+      <ul>
+        <div className="weather-app-wrapper">
+          <li className="col-3">Feels like: {weatherData.feels_like}°C</li>
+          <li className="col-3">
+            H/L: <span className="high-temp">{weatherData.max_temp}</span>°C
+            <span className="low-temp"> {weatherData.min_temp}</span>°C
+          </li>
+          <li className="col-3">Humidity: {weatherData.humidity}%</li>
+          <li className="col-3">Winds: {weatherData.wind} km/h</li>
+        </div>
+      </ul>
+      <br />
+    );
+    } else {
+      let apiKey = "ce4afc8744a0f1fef5bae5142b3fbd94";
+      let city = "Valencia";
+      let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      axios.get(apiUrl).then(handleResponse);
+      
+      return "Load";
+}
+ 
+   
 
         {/*   **********  Weather Forecast  **********  */}
         <div className="row weather-forecast"></div>
@@ -101,4 +121,5 @@ export default function Weather() {
       </div>
     </div>
   );
+}
 }
